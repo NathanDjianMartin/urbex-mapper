76c0ce5589e60e26cd98746c4b668a8943f161b8
NathanDjianMartin <ndmroot-git@yahoo.fr>
2023-01-22 19:34:25 +0100

    feat(map): basic map that adds markers on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import React from 'react'
 import './App.css'
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
-import React from 'react';
 import Map from './Map'
+import { Marker } from './Marker'
 
 function App() {
+  const [clicks, setClicks] = React.useState<google.maps.LatLng[]>([])
+  const [zoom, setZoom] = React.useState(16)
+  const [center, setCenter] = React.useState<google.maps.LatLngLiteral>({
+    lat: 0,
+    lng: 0,
+  })
+
+  const onClick = (event: google.maps.MapMouseEvent) => {
+    setClicks([...clicks, event.latLng!]) // TODO: handle exclamation mark gracefully
+  }
+
+  const onIdle = (map: google.maps.Map) => {
+    setZoom(map.getZoom()!) // TODO: handle exclamation mark gracefully
+    setCenter(map.getCenter()!.toJSON()) // TODO: handle exclamation mark gracefully
+  }
+
   return (
-    <div className="App">
+    <div className="App" style={{height: "1000px", width: "1000px", background: "red"}}>
       <h1>Urbex Mapper</h1>
-      <Map />
+      <Map style={{"margin": "10px"}}  center={center} zoom={zoom} onClick={onClick} onIdle={onIdle}>
+        {clicks.map((latLng, index) => (<Marker key={index} position={latLng} />))}
+      </Map>
     </div>
   )
 }
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,24 +1,105 @@
 import './App.css'
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import React from 'react';
+import { isLatLngLiteral } from "@googlemaps/typescript-guards";
+import { createCustomEqual } from 'fast-equals';
 
-function Map() {
+interface MapProps extends google.maps.MapOptions {
+  style: { [key: string]: string };
+  onClick?: (e: google.maps.MapMouseEvent) => void;
+  onIdle?: (map: google.maps.Map) => void;
+  children?: React.ReactNode;
+}
+
+const Map: React.FC<MapProps> = ({
+  onClick,
+  onIdle,
+  children,
+  style,
+  ...options
+}) => {
 
   const ref = React.useRef<HTMLDivElement>(null);
   const [map, setMap] = React.useState<google.maps.Map>();
 
+  useDeepCompareEffectForMaps(() => {
+    if (map) {
+      map.setOptions(options)
+    }
+  }, [map, options])
+
   React.useEffect(() => {
     if (ref.current && !map) {
-      setMap(new window.google.maps.Map(ref.current, {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-      }));
+      setMap(new window.google.maps.Map(ref.current, {}));
     }
   }, [ref, map]);
 
+  // clears existing listeners and replace them when a handler is updated
+  React.useEffect(() => {
+    if (map) {
+      ["click", "idle"].forEach((eventName) => {
+        google.maps.event.clearListeners(map, eventName)
+      });
+
+      if (onClick) {
+        map.addListener("click", onClick)
+      }
+
+      if (onIdle) {
+        map.addListener("idle", () => onIdle(map));
+      }
+    }
+  }, [map, onClick, onIdle]);
+
   return (
-    <div id="map" ref={ref} />
+    <div style={{height: "100px", width: "100px"}}>
+      <div>Test</div>
+      <div ref={ref} style={{...style, height: "1000px", width: "1000px"}} />
+      {
+        React.Children.map(children, (child) => {
+          if (React.isValidElement(child)) {
+            // set map prop on child component
+            // @ts-ignore
+            return React.cloneElement(child, { map }) // TODO: use renderItem prop instead https://beta.reactjs.org/reference/react/cloneElement
+          }
+        })
+      }
+    </div>
   )
 }
 
 export default Map
+
+const deepCompareEqualsForMaps = createCustomEqual(
+  (deepEqual) => (a: any, b: any) => {
+    if (
+      isLatLngLiteral(a) ||
+      a instanceof google.maps.LatLng ||
+      isLatLngLiteral(b) ||
+      b instanceof google.maps.LatLng
+    ) {
+      return new google.maps.LatLng(a).equals(new google.maps.LatLng(b));
+    }
+
+    // TODO extend to other types
+
+    // use fast-equals for other objects
+    return deepEqual(a, b);
+  }
+);
+
+function useDeepCompareMemoize(value: any) {
+  const ref = React.useRef();
+
+  if (!deepCompareEqualsForMaps(value, ref.current)) {
+    ref.current = value;
+  }
+
+  return ref.current;
+}
+
+function useDeepCompareEffectForMaps(
+  callback: React.EffectCallback,
+  dependencies: any[]
+) {
+  React.useEffect(callback, dependencies.map(useDeepCompareMemoize));
+}
